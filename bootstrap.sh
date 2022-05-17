5aff2c42eadf43ae5a599c5b547f3195e72fddfe
maddygoround <rathodm63@gmail.com>
2022-05-17 12:58:41 +0530

    Bootstrap script updated to correct the .env path

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 docker-compose up -d
 consul_address=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' consul)
-rm ./config/.env && touch ./config/.env
-echo CONSUL_IP=$consul_address >> ./config/.env
+rm ./nodes/.env && touch ./nodes/.env
+echo CONSUL_IP=$consul_address >> ./nodes/.env
